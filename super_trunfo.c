0f7c610d2945da59a614e0f491198d26acb5af50
GuiDev8 <137966208+GuiDev8@users.noreply.github.com>
2025-04-15 16:56:19 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,60 +1,62 @@
-#include <stdio.h>
+#include  <stdio.h> 
 
-int main(){
+int main () {
 
-    char estado[50], codigodacarta[10], nomedacidade[50];
-    int populacao, numerodepontosturisticos;
-    float pib, area;
+    char estado1 = 'A';
+    char carta1 [10] = "A01";
+    char cidade1 [20] = "São Paulo";
+    int populacao1 = 5898000;
+    float area1 = 1202222.0;
+    float pib1 = 12345267.0;
+    int pontosturisticos1 = 32;
 
-    /*Para que o código rode normalmente, peço que não utilize ESPAÇO.
-    Utilizar a barra de ESPAÇO nas respostas, faz com que o programa entanda que você finalizou a resposta.
-    O que gera erros no programa. Grato :D .*/
+    float densidadepopulacional1 = populacao1 / area1;
 
-    printf("Primeira Carta \n");
+    float pibpercapita1 = pib1 / populacao1;
 
-    printf("Estado: \n"); 
-    scanf("%s", &estado);
 
-    printf("Digite o Código da carta: \n");
-    scanf("%s", &codigodacarta);
+    printf("***Carta número 1***\n");
 
-    printf("Nome da Cidade: \n"); 
-    scanf("%s", &nomedacidade);
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", carta1);
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("Pib: %.2f\n", pib1);
+    printf("Pontos Turisticos: %d\n", pontosturisticos1);
 
-    printf("Digite a População: \n");
-    scanf("%d", &populacao);
+    printf("Densidade populacional: %.2f\n", densidadepopulacional1);
 
-    printf("Número de Pontos Túristicos: \n");
-    scanf("%d", &numerodepontosturisticos);
+    printf("PIB per Capita: %.2f\n", pibpercapita1);
 
-    printf("Digite a Área da cidade em km²: \n");
-    scanf("%f", &area);
 
-    printf("Digite o PIB do Estado: \n");
-    scanf("%f", &pib);
+    char estado2 = 'A';
+    char carta2 [10] = "A02";
+    char cidade2 [20] = "Campinas";
+    int populacao2 = 4897000;
+    float area2 = 120111.0;
+    float pib2 = 12345212.0;
+    int pontosturisticos2 = 18;
 
-    printf("Segunda carta \n");
+    float densidadepopulacional2 = populacao2 / area2;
 
-    printf("Estado: \n"); 
-    scanf("%s", &estado);
+    float pibpercapita2 = pib2 / populacao2;
 
-    printf("Digite o Código da carta: \n");
-    scanf("%s", &codigodacarta);
 
-    printf("Nome da Cidade: \n"); 
-    scanf("%s", &nomedacidade);
+    printf("***Carta número 2***\n");
 
-    printf("Digite a População: \n");
-    scanf("%d", &populacao);
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", carta2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("Pib: %.2f\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontosturisticos2);
 
-    printf("Número de Pontos Túristicos: \n");
-    scanf("%d", &numerodepontosturisticos);
+    printf("Densidade populacional: %.2f\n", densidadepopulacional2);
 
-    printf("Digite a Área da cidade em km²: \n");
-    scanf("%f", &area);
+    printf("PIB per Capita: %.2f\n", pibpercapita2);
 
-    printf("Digite o PIB do Estado: \n");
-    scanf("%f", &pib);
 
     return 0;
 
